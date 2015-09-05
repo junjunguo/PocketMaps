7d7c843b415cc369a08fd6bf3d97ca01376f4ad0
junjunguo <guojunjun@gmail.com>
2015-09-06 01:46:51 +0200

    add rating & home page link

--- PocketMaps/app/src/main/java/com/junjunguo/pocketmaps/controller/MainActivity.java
+++ PocketMaps/app/src/main/java/com/junjunguo/pocketmaps/controller/MainActivity.java
@@ -1,9 +1,11 @@
 package com.junjunguo.pocketmaps.controller;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -297,6 +299,25 @@ public class MainActivity extends AppCompatActivity
             case R.id.menu_settings:
                 //                got to setting;
                 startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            case R.id.menu_home_page:
+                //                got to home page;
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://junjunguo.com/PocketMaps/")));
+                return true;
+            case R.id.menu_rate_pocket_maps:
+                Uri uri = Uri.parse("market://details?id=" + context.getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                // To count with Play market backstack, After pressing back button,
+                // to taken back to our application, we need to add following flags to intent.
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                        Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET |
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" + context.getPackageName())));
+                }
                 return true;
             case R.id.menu_quit:
                 quitApp();
