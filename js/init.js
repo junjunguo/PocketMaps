5ded5c7fcac061bccd2cd0ccf950a04c51284caf
Junjun Guo <guojunjun@gmail.com>
2016-01-24 20:50:43 +0100

    update web: change new google play badge: add annimation

--- js/init.js
+++ js/init.js
@@ -6,3 +6,23 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+// load js/wow.min.js
+var wow = new WOW(
+    {
+      boxClass:        'wow',      // animated element css class (default is wow)
+      animateClass:    'animated', // animation css class (default is animated)
+      offset:          0,          // distance to the element when triggering the animation (default is 0)
+      mobile:          true,       // trigger animations on mobile devices (default is true)
+      live:            true,       // act on asynchronously loaded content (default is true)
+      callback:        function (box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null // optional scroll container selector, otherwise use window
+    }
+);
+
+wow.init();
