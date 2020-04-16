6e3cd17eb0b14da7b2d267cdec03f4a7becfe955
Starcommander <starcommander@gmx.at>
2020-04-17 00:10:27 +0200

    Include signing into script.

--- mapGenerator/transfer_playstore.sh
+++ mapGenerator/transfer_playstore.sh
@@ -11,7 +11,9 @@
 ##
 ##############################################################################
 
-PROJ_PATH="/home/ppp/Desktop/Programmieren/map/PocketMaps/PocketMaps/"
+PROJ_PATH="/home/ppp/workspace/NetBeansProjects/PocketMaps_git/PocketMaps/"
+RELEASE_KEY="/home/ppp/workspace/Android-SDK/key_google/google-release-key.keystore"
+ANDROID_SDK="/home/ppp/workspace/Android-SDK/android-sdk-linux/"
 
 do_transfer()
 {
@@ -31,15 +33,25 @@ do_transfer_back()
   find app/src/main/java/ -name "*.java" -print0 | xargs -0 --max-args 1 --replace="{}" sed -i -e "s/com.starcom.pocketmaps/com.junjunguo.pocketmaps/g" "{}"
 }
 
+do_sign()
+{
+  jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$RELEASE_KEY" $PROJ_PATH/app/build/outputs/apk/release/app-release-unsigned.apk starcommander
+  $ANDROID_SDK/build-tools/27.0.1/zipalign -v -p 4 $PROJ_PATH/app/build/outputs/apk/release/app-release-unsigned.apk $PROJ_PATH/app/build/outputs/apk/release/app-release.apk
+}
+
 if [ -z "$1" ]; then
   echo "Use arg t=transfer to transfer for playstore"
   echo "Use arg b=back to transfer back"
+  echo "Use arg s=sign to sign the apk"
 elif [ "$1" = "t" ]; then
   do_transfer
-  echo "Finish! Now clear tmp data, and build again."
+  echo "Finish! Now build again."
 elif [ "$1" = "b" ]; then
   do_transfer_back
-  echo "Finish! Now clear tmp data, and build again."
+  echo "Finish! Now build again."
+elif [ "$1" = "s" ]; then
+  do_sign
+  echo "Finish! Created file: app-release.apk"
 else
   echo "Wrong argument!"
 fi
